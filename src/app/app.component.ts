0ca824470360a05ab51b8a90b79edfeab0530fe2
SJCMN <sam.clavette@gmail.com>
2022-02-22 09:48:15 -0600

    add pipe numbers, add custom convertMiles pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,13 @@ export class AppComponent {
  paymentDate: string = '';
  amount: number = 0 ;
  height: number = 0;
+ miles: number = 0;
+
+ car = {
+   make: 'Toyota',
+   model: 'Camry',
+   year: 1993
+ }
 
  onNameChange(value: string){
    this.name = value;
@@ -25,5 +32,9 @@ export class AppComponent {
 
  onHeightChange(value: string){
    this.height = parseFloat(value)
+ }
+
+ onMilesChange(value: string){
+   this.miles = parseFloat(value)
  }
 }
